c027e7040b210e3b23ac78bb94323660bd141aa3
Sam <sam.dealy@gmail.com>
2019-02-05 23:12:01 -0800

    Update naming and add comments

--- javascript/parser.js
+++ javascript/parser.js
@@ -1,7 +1,7 @@
 Papa.parse("https://docs.google.com/spreadsheets/d/e/2PACX-1vRCmu8vTzD_R7L2iqEE0gdD43zbEnTUv5-_f6cHz1zX16JN6c2sdWKagLuOWPO8HBnbghfmInxWNwSz/pub?output=csv", {
 	download: true,
 	complete: results => {
-		findFileNameAndCalculateTimeToStartPlaying(results);
+		findFileNameAndCalculatePlaybackStartTime(results);
 	}
 });
 
@@ -12,9 +12,9 @@ Papa.parse("https://docs.google.com/spreadsheets/d/e/2PACX-1vRCmu8vTzD_R7L2iqEE0
 	source URL for the clip. The function then finds at which time to
 	play the clip.
 
-	
+
 */
-function findFileNameAndCalculateTimeToStartPlaying(results) {
+function findFileNameAndCalculatePlaybackStartTime(results) {
 	const date = new Date();
 	const clipDataObjectsArray = createClipDataObjectsArray(results);
 
@@ -24,7 +24,7 @@ function findFileNameAndCalculateTimeToStartPlaying(results) {
 	const partNumber = currentClipDataObject.partNumber;
 
  	// TODO: figure out if the playback time should be in seconds or milliseconds
-	const timeToStartPlayingVideo = calculateTimeToStartPlayingClip(partNumber, date);
+	const timeToStartPlayingVideo = calculatePlaybackStartTime(partNumber, date);
 
 	console.log(`File name is: ${fileName}`);
 	console.log(`Time to start playing video is: ${timeToStartPlayingVideo}`);
@@ -38,19 +38,38 @@ function findFileNameAndCalculateTimeToStartPlaying(results) {
 */
 function findCurrentClipDataObject(clipDataObjectsArray, date) {
 	const minutesPastMidnight = calculateMinutesPastMidnight(date);
-	const indexOfFile = Math.floor(minutesPastMidnight / 15);
-	const currentClipDataObject = clipDataObjectsArray[indexOfFile];
+
+	// Tricky logic:
+	// 		1) There are 96 time slots per day. Starts at 0:00 and increments
+	// 			 by 15 minutes until you get to the last one 23:45
+	//       (0:00, 0:15, 0:30 ... 23:30, 23:45)
+	// 		2) We can then map the minutes past midnight to the array index
+	//       by flooring the quotient of minutesPastMidnight and 15.
+	//        Note: To prove that this mapping works, let's use an example.
+	//            --The fourth timeslot of the day starts at 0:45.
+  //            			-- It goes from 0:45 up until 1:00.
+	//						--The fourth element in the clipDataObjectsArray is the
+	//            currentClipDataObject for this timeSlot.
+	//            -- The fourth element in the clipDataObjectsArray occurs
+	//               at index 3.
+	//            -- For all the minutes in the range 0:45 (inclusive)
+	//               and 1:00 (un-inclusive),
+	//               Math.floor(time / 15) === 3
+	//            -- This logic applies to any timeslot. Try it out on
+	//               other timeslots to to convince yourself
+	const indexOfCurrentClipDataObject = Math.floor(minutesPastMidnight / 15);
+	const currentClipDataObject = clipDataObjectsArray[indexOfCurrentClipDataObject];
 
 	return currentClipDataObject;
 }
 
-function calculateTimeToStartPlayingClip(partNumber, date) {
+function calculatePlaybackStartTime(partNumber, date) {
 	const minutes = date.getMinutes();
 	const seconds = date.getSeconds();
 
 	const playbackMinutes = (partNumber - 1) * 15 + minutes % 15;
-
 	const playbackSeconds = playbackMinutes * 60 + seconds;
+
 	return playbackSeconds;
 }
 
