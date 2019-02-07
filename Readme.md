4e479a35d35eeffe83f276e77018aac6975286e0
Sam <sam.dealy@gmail.com>
2019-02-06 20:29:15 -0800

    Add Readme

---
+++ Readme.md
@@ -0,0 +1,13 @@
+<h2>Note about logic in findCurrentClipDataObject function</h2>
+1. There are 96 time slots per day. Starts at 0:00 and increments
+by 15 minutes until you get to the last one 23:45 (0:00, 0:15, 0:30 ... 23:30, 23:45)
+2. We can then map the minutes past midnight to the array index
+by flooring the quotient of minutesPastMidnight and 15.
+
+To prove that this mapping works, let's use an example.
+1. The fourth timeslot of the day starts at 0:45. It goes from 0:45 up until 1:00.
+2. The fourth element in the clipDataObjectsArray is the currentClipDataObject for this timeSlot.
+3. The fourth element in the clipDataObjectsArray occurs at index 3.
+4. For all the minutes in the range 0:45 (inclusive) and 1:00 (un-inclusive):
+    *  Math.floor(time / 15) === 3
+5. This logic applies to any timeslot. Try it out oN other timeslots to to convince yourself
--- javascript/parser.js
+++ javascript/parser.js
@@ -39,26 +39,8 @@ function findFileNameAndCalculatePlaybackStartTime(results) {
 function findCurrentClipDataObject(clipDataObjectsArray, date) {
 	const minutesPastMidnight = calculateMinutesPastMidnight(date);
 
-	// Tricky logic:
-	// 		1) There are 96 time slots per day. Starts at 0:00 and increments
-	// 			 by 15 minutes until you get to the last one 23:45
-	//       (0:00, 0:15, 0:30 ... 23:30, 23:45)
-	// 		2) We can then map the minutes past midnight to the array index
-	//       by flooring the quotient of minutesPastMidnight and 15.
-	//        Note: To prove that this mapping works, let's use an example.
-	//            --The fourth timeslot of the day starts at 0:45.
-  //            			-- It goes from 0:45 up until 1:00.
-	//						--The fourth element in the clipDataObjectsArray is the
-	//            currentClipDataObject for this timeSlot.
-	//            -- The fourth element in the clipDataObjectsArray occurs
-	//               at index 3.
-	//            -- For all the minutes in the range 0:45 (inclusive)
-	//               and 1:00 (un-inclusive),
-	//               Math.floor(time / 15) === 3
-	//            -- This logic applies to any timeslot. Try it out on
-	//               other timeslots to to convince yourself
-	const indexOfCurrentClipDataObject = Math.floor(minutesPastMidnight / 15);
-	const currentClipDataObject = clipDataObjectsArray[indexOfCurrentClipDataObject];
+	const indexOfCurrentClipObject = Math.floor(minutesPastMidnight / 15);
+	const currentClipDataObject = clipDataObjectsArray[indexOfFile];
 
 	return currentClipDataObject;
 }
