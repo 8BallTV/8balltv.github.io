60922e61477a67aadaa327064ae36de7c58d6423
Sam <sam.dealy@gmail.com>
2019-02-09 12:25:45 -0800

    Document formatParsedata

--- javascript/format_parse_data.js
+++ javascript/format_parse_data.js
@@ -1,3 +1,11 @@
+/*
+* Transforms the csvParseResults into an array of clipDataObjects. The
+* clipDataObject keys correspond to the CSV file's column headers.
+
+* @param {Array<Array<String>>} csvParseResults
+* @return {Array<Object>} clipDataObjectsArray
+*/
+
 export function formatParseData(csvParseResults) {
   const clipDataObjectsArrayWithTitle = csvParseResults.data.map((data, i) => {
     return {
@@ -8,8 +16,8 @@ export function formatParseData(csvParseResults) {
       director: data[4]
     };
   });
-  // Slice to get rid of the first entry, which is the CSV's column
-  // title
+  // Slice to get rid of the first entry,
+  // which is the CSV's column title
   const clipDataObjectsArray = clipDataObjectsArrayWithTitle.slice(1);
   return clipDataObjectsArray;
 }
