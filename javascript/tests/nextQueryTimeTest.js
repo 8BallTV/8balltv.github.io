23834b2e5bdc217ba45d5002eab4a4da575ca03a
Sam <sam.dealy@gmail.com>
2019-02-08 18:44:55 -0800

    Finish tests for time

--- javascript/tests/nextQueryTimeTest.js
+++ javascript/tests/nextQueryTimeTest.js
@@ -7,11 +7,11 @@ batchTestRunner();
 function generateTestCases() {
   const testCases = [
     [ new Date('Thu, 01 Jan 2019 08:02:36'), 744],
-    [ new Date('Thu, 01 Jan 2019 04:42:17'), ],
-    [ new Date('Thu, 01 Jan 2019 12:00:00'), ],
-    [ new Date('Thu, 01 Jan 2019 12:35:30'), ],
-    [ new Date('Thu, 01 Jan 2019 11:59:59'), ],
-    [ new Date('Thu, 01 Jan 2019 01:30:00'), ],
+    [ new Date('Thu, 01 Jan 2019 04:42:17'), 60 - 17 + (45 - 43) * 60],
+    [ new Date('Thu, 01 Jan 2019 12:00:00'), 15 * 60 ],
+    [ new Date('Thu, 01 Jan 2019 12:35:30'), 30 + (45 - 36) * 60],
+    [ new Date('Thu, 01 Jan 2019 11:59:59'), 1 ],
+    [ new Date('Thu, 01 Jan 2019 01:30:00'), 15 * 60 ],
   ]
 
   return testCases;
@@ -33,5 +33,5 @@ function batchTestRunner() {
 function generateErrorMessages(expectedTime, actualTime) {
   const colorString = "color:red";
   console.log(`    %cExpected time was: ${expectedTime}`, colorString);
-  console.log(`    %cActual file name was: ${actualTime}`, colorString);
+  console.log(`    %cActual time was: ${actualTime}`, colorString);
 }
