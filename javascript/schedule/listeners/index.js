ce441ff9d111121bd85541a0522dd238264ac182
Sam <sam.dealy@gmail.com>
2019-02-23 13:00:57 -0800

    Add documentation

--- javascript/schedule/listeners/index.js
+++ javascript/schedule/listeners/index.js
@@ -1,5 +1,9 @@
 import registerSchedulesByDayLinksListener from './schedule_by_day.js';
 
+/*
+* Register all listeners for the schedule page (schedule.html)
+* @param{null}, @return{null}
+*/
 export default function registerListeners() {
   registerSchedulesByDayLinksListener();
 }
--- javascript/video_page/listeners/index.js
+++ javascript/video_page/listeners/index.js
@@ -1,6 +1,10 @@
 import registerFullScreenListeners from './fullscreen.js';
 import registerSoundListeners from './sound.js';
 
+/*
+* Register all listeners for the video_page (index.html)
+* @param{null}, @return{null}
+*/
 export default function registerListeners() {
   registerFullScreenListeners();
   registerSoundListeners();
