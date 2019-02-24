f8b430a21b4bb2cafcf4a8b7345105ff60e8d0eb
Sam <sam.dealy@gmail.com>
2019-02-23 22:10:05 -0800

    Reorder imports

--- javascript/schedule/index.js
+++ javascript/schedule/index.js
@@ -1,9 +1,9 @@
-import scheduleClockUpdate from './clock.js';
-import parseTSV from '../parser/index.js';
-import renderTitlesOnSchedule from './create_schedule.js';
 import registerListeners from './listeners/index.js';
+import scheduleClockUpdate from './clock.js';
 import { setSelectedCSS } from './create_schedule.js';
 import scheduleSetNowTextOnCurrentClipQuarter  from './set_now.js';
+import parseTSV from '../parser/index.js';
+import renderTitlesOnSchedule from './create_schedule.js';
 
 registerListeners();
 scheduleClockUpdate();
