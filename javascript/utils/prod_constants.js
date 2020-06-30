24d2b78d6aae6af3c0d1e38a5d5638f5d081a5c4
Sam <sam.dealy@gmail.com>
2020-06-29 23:19:50 -0700

    Use cors-anywhere to solve new Google Sheets CORS anywhere

--- javascript/utils/prod_constants.js
+++ javascript/utils/prod_constants.js
@@ -1,7 +1,9 @@
 /** To generate these URLs, ensure that your Google Sheet is published to the web. */
 /** @const @type {String} */
+//TODO(samdealy): Keep an eye on the rate limit for cors-anywhere. Potentially
+// deploy our own heroku app that does this.
 const FIRST_PART_OF_SCHEDULE_URL_PROD =
-    "https://docs.google.com/spreadsheets/d/e/2PACX-1vQ7EnR4fHqDgT3ZqXWXpl4xb7C4tv1PkEMHfxN5sGHponRwH36OALT_MrY3JLn3zPyrqr0oERerBob6/pub?gid=";
+  "https://cors-anywhere.herokuapp.com/https://docs.google.com/spreadsheets/d/e/2PACX-1vQ7EnR4fHqDgT3ZqXWXpl4xb7C4tv1PkEMHfxN5sGHponRwH36OALT_MrY3JLn3zPyrqr0oERerBob6/pub?gid=";
 /** @const @type {String} */
 const SECOND_PART_OF_SCHEDULE_URL_PROD = "&single=true&output=tsv";
 
@@ -12,15 +14,15 @@ const SECOND_PART_OF_SCHEDULE_URL_PROD = "&single=true&output=tsv";
  */
 
 const TSV_SHEET_IDS_PROD = {
-    monday: "192360482",
-    tuesday: "1036455868",
-    wednesday: "949173297",
-    thursday: "483801566",
-    friday: "228299691",
-    saturday: "1694530893",
-    sunday: "835076295",
-    collectionVideos: "681715567",
-    collectionInfo: "881663141",
+  monday: "192360482",
+  tuesday: "1036455868",
+  wednesday: "949173297",
+  thursday: "483801566",
+  friday: "228299691",
+  saturday: "1694530893",
+  sunday: "835076295",
+  collectionVideos: "681715567",
+  collectionInfo: "881663141",
 };
 
 /**
@@ -29,8 +31,8 @@ const TSV_SHEET_IDS_PROD = {
  * @description Used to determine each day's prod schedule URL
  */
 const PROD_CONSTANTS = [
-    FIRST_PART_OF_SCHEDULE_URL_PROD,
-    SECOND_PART_OF_SCHEDULE_URL_PROD,
-    TSV_SHEET_IDS_PROD,
+  FIRST_PART_OF_SCHEDULE_URL_PROD,
+  SECOND_PART_OF_SCHEDULE_URL_PROD,
+  TSV_SHEET_IDS_PROD,
 ];
 export default PROD_CONSTANTS;
